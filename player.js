f405d5a97544bbcd0e3c6ea607b267bf2d54bfee
IoanaAndra <ioanaandrapopovici@gmail.com>
2020-02-24 23:58:56 +0000

    adding parameters to Player

--- player.js
+++ player.js
@@ -1,4 +1,9 @@
-function Player() {
+function Player(classType, health, mana, strength, agility, speed) {
 
-    
+    this.classType = classType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
 }
