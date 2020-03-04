bd9ca2003db0d4410f2061a862ae800f7f7bc6c9
IoanaAndra <ioanaandrapopovici@gmail.com>
2020-03-04 17:12:26 +0000

    added swtich case when user chooses the player

--- gamemanager.js
+++ gamemanager.js
@@ -1,17 +1,46 @@
 let GameManager = {
 
- setGameStart: function() {
+ setGameStart: function(clasType) {  //needs parameter type string
 
-    this.resetPlayer();
+    this.resetPlayer(classType);
     this.setPreFight(); //onclick can only take one method/function
 
  },
 
- resetPlayer: function()  {
+ resetPlayer: function(classType)  {
 
- },
+   switch (classType) {
+         case "Warrior":
+         
+         player = new Player(classType, 200, 0, 200, 100, 50);
 
- setPreFight: function() {4
+         break;
+   
+         case "Rogue":
+         
+         player = new Player(classType, 100, 0, 200, 100, 50);
+      
+         break;
+               
+         case "Mage":
+         
+         player = new Player(classType, 100, 0, 200, 100, 50);
+
+         break;
+         
+         case "Hunter":
+         
+         player = new Player(classType, 200, 0, 200, 100, 50);
+      
+         break;
+
+   }
+let getInterface = document.querySelector(".interface")
+ getInterface.innerHTML = "";
+
+},
+
+ setPreFight: function() {
 
 }
 
